223f3332c6703f6f9636c2b283e6a8d63bcc24e3
Sarah Wood <wood.sarah@outlook.com>
2020-09-09 17:01:04 +0100

    Update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import App from './App.svelte';
 
-const app = mew App({
-  target: document.body,
-  props: {
-    answer: 42
-  }
+const app = new App({
+	target: document.body,
+	props: {
+		name: 'world'
+	}
 });
+
+export default app;
